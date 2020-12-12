9087d2fb9d8b3afe09377f31bf8ec37cb8429e38
Thanh <kentkillboss@gmail.com>
2020-12-12 09:48:00 +0700

    update thanh

---
+++ backend/BusinessObject.py
@@ -0,0 +1,12 @@
+class Customer:
+    def __init__(self,CustomerID,CustomerName,ContactName,Address,City,PostalCode,Country)
+        self.CustomerID = CustpmerID
+        self.CustomerName = CustomerName
+        slef.ContactName = ContactName
+        self.Address = Address
+        self.City = City
+        slef.PostalCode = postalCode
+        self.Country = Country
+
+if __name__ == "__main__":
+    print('this is business object package')

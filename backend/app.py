8dd295ac948b55186f2eb3c52dbfd79990e3a6df
Thanh <kentkillboss@gmail.com>
2020-12-13 16:08:27 +0700

    update

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,28 @@
 from flask import Flask
-
+import os
+import BusinessObject as bo
+import DataObjects as do
 
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
+dp_ip = os.getenv("db_ip")
+ConnectionData = {}
+ConectionData['user' = 'postgres']
+ConectionData['password' = 'password']
+ConnectionData['host'] = str(db_ip)
+ConnectionData['port'] = '5432'
+ConnectionData['database'] = 'teamhhl'
+
+@app.route("/")
+def hello():
+    c1 = bo.Customer(1,'DAU xanh','Peter','566 Nui Thanh', 'Da Nang','5000','VietNam')
+    return c1.CustomerName
+@app.route("/test_insert")
+def test_inset():
+    c2 = do.Customer(ConnectionData)
+    c1 = bo.Customer(1,'DAU xanh','Peter','566 Nui Thanh', 'Da Nang','5000','VietNam')
+    s1 = c2.insert(c1)
+    return s1
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)

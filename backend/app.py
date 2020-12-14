64539198e1bbfea7f73749647a3a8c0c7f01601a
Thanh <kentkillboss@gmail.com>
2020-12-14 12:21:16 +0700

    a

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,27 @@
 from flask import Flask
-import os
-import BusinessObject as bo
-import DataObjects as do
+# import os
+# import BusinessObject as bo
+# import DataObjects as do
 
 app = Flask(__name__)
 
-dp_ip = os.getenv("db_ip")
-ConnectionData = {}
-ConnectionData['user' = 'postgres']
-ConnectionData['password' = 'postgres']
-ConnectionData['host'] = str(db_ip)
-ConnectionData['port'] = '5432'
-ConnectionData['database'] = 'northwind'
+# dp_ip = os.getenv("db_ip")
+# ConnectionData = {}
+# ConnectionData['user' = 'postgres']
+# ConnectionData['password' = 'postgres']
+# ConnectionData['host'] = str(db_ip)
+# ConnectionData['port'] = '5432'
+# ConnectionData['database'] = 'northwind'
 
 @app.route("/")
 def hello():
    return "hello"
-@app.route("/test_insert")
-def test_inset():
-    c2 = do.Customer(ConnectionData)
-    c1 = bo.Customer(1,'DAU xanh','Peter','566 Nui Thanh', 'Da Nang','5000','VietNam')
-    s1 = c2.insert(c1)
-    return s1
+# @app.route("/test_insert")
+# def test_inset():
+#     c2 = do.Customer(ConnectionData)
+#     c1 = bo.Customer(1,'DAU xanh','Peter','566 Nui Thanh', 'Da Nang','5000','VietNam')
+#     s1 = c2.insert(c1)
+#     return s1
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)

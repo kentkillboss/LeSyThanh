7fc09081b9c149b988c69f4d64cd975409e0877e
Thanh <kentkillboss@gmail.com>
2020-12-18 19:21:16 +0700

    add cus

--- backend/test/test.py
+++ backend/test/test.py
@@ -10,10 +10,20 @@ import requests
 # rsp = requests.post('http://192.168.1.11:8080/test_send_receive', json=data)
 # print(rsp.text)
 
+# data = {}
+# data['CustomerID'] = 1
+# data['CustomerName'] = 'FTP'
+# data['ContactName'] = 'Sy Thanh'
+# data['Address'] = 'Da Nang'
+# data['City'] = 'Vip'
+# data['PostalCode'] = '123456'
+# data['Country'] = 'Vietnam'
+# rsp = requests.post('http://192.168.1.11:8080/user/insert', json=data)
+# print(rsp.text)
 data = {}
 data['CustomerID'] = 1
-data['CustomerName'] = 'FTP'
-data['ContactName'] = 'Sy Thanh'
+data['CustomerName'] = 'Tokyo'
+data['ContactName'] = 'An Ta'
 data['Address'] = 'Da Nang'
 data['City'] = 'Vip'
 data['PostalCode'] = '123456'
